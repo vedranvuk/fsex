f9859ea57ab56f919587017ad4e9a8552a4588f8
vedranvuk <vedranvuk82@gmail.com>
2020-11-24 10:24:37 +0100

    Added RWFS and RWFile

---
+++ rwfs.go
@@ -0,0 +1,19 @@
+// Copyright 2020 Vedran Vuk. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package fsex
+
+import "github.com/vedranvuk/fs"
+
+// RWFS is a fs.FS that opens files with write access.
+type RWFS interface {
+	Open(name string) (RWFile, error)
+}
+
+// RWFile is a fs.File with Write and Seek methods.
+type RWFile interface {
+	fs.File
+	Write([]byte) (int, error)
+	Seek(int64, int) (int64, error)
+}
